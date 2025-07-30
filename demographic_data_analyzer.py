6fbe9de1bb9cd7ed772bd5eac121e57b55876f6f
Tristan Martin <157137017+TJFM4@users.noreply.github.com>
2025-07-30 11:25:33 -0400

    read csv

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -1,9 +1,8 @@
 import pandas as pd
 
-
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = pd.read_csv(adult.data.csv)
+    df = pd.read_csv('./adult.data.csv')
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
     race_count = None
@@ -67,3 +66,5 @@ def calculate_demographic_data(print_data=True):
         highest_earning_country_percentage,
         'top_IN_occupation': top_IN_occupation
     }
+
+calculate_demographic_data()
